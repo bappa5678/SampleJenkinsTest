688d58b1995156591cadd82050657943d1e95e36
Bappaditya <bappa.5678@gmail.com>
2020-09-14 00:03:08 +0530

    new file

--- src/test/java/com/jenkins/testpkg/SampleJenkins.java
+++ src/test/java/com/jenkins/testpkg/SampleJenkins.java
@@ -1,9 +1,15 @@
 package com.jenkins.testpkg;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class SampleJenkins {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println("This is jenkins project");
+		if(!Files.exists(Paths.get("src/test/resources/emailText.txt")))
+        	Files.createFile(Paths.get("src/test/resources/emailText.txt"));
 
 	}
 
