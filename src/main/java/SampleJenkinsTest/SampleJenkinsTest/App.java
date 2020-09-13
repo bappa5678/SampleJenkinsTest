1d8942bd7ee3ee7ff0215e5fd20083a4acf060cc
Bappaditya <bappa.5678@gmail.com>
2020-09-13 23:40:33 +0530

    updating sample code

---
+++ src/main/java/SampleJenkinsTest/SampleJenkinsTest/App.java
@@ -0,0 +1,13 @@
+package SampleJenkinsTest.SampleJenkinsTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/jenkins/testpkg/SampleJenkins.java
@@ -0,0 +1,10 @@
+package com.jenkins.testpkg;
+
+public class SampleJenkins {
+
+	public static void main(String[] args) {
+		System.out.println("This is jenkins project");
+
+	}
+
+}
